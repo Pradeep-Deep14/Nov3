ba38e3fc0e2e5aa367081756b4073382f76d7229
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-11-03 09:18:26 +0530

    first commit

---
+++ 3.py
@@ -0,0 +1,16 @@
+def my_family(age):
+    family={
+        'Dad':range(40,100),
+        'Mom':range(50),
+        'Child':range(0,18)
+    }
+
+    for member,age_range in family.items():
+        if age in age_range:
+            return member
+        
+    return "Unknown"
+
+print(my_family(45))
+
+#Dad
